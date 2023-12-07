931719db3239759fe7054c1e1b2d806e67954732
kaji shuma <j1912@matsue-ct.ac.jp>
2023-12-07 14:28:00 +0900

    git表示追加

--- j1912_kadai2.c
+++ j1912_kadai2.c
@@ -2,11 +2,13 @@
 
 int main()
 {
-  printf("fizzbuzz checked start!\n");
+  printf("fizzbuzz and git checked start!\n");
   for (int i = 1; i <= 30; i++)
   {
     if (i % 3 == 0 && i % 5 == 0)
       printf("fizzbuzz\n");
+    else if (i % 7 == 0)
+      printf("git\n");
     else if (i % 3 == 0)
       printf("fizz\n");
     else if (i % 5 == 0)
@@ -14,6 +16,6 @@ int main()
     else
       printf("%d is not zz\n", i);
   }
-  printf("fizzbuzz checked cleared all!\n");
+  printf("fizzbuzz and git checked cleared all!\n");
   return 0;
 }
