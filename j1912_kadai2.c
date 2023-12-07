6780048c6b98174e524cb0b15e3bf6c792bcebfc
kaji shuma <j1912@matsue-ct.ac.jp>
2023-12-07 13:44:05 +0900

    fizz, buzzそれぞれの表示追加

--- j1912_kadai2.c
+++ j1912_kadai2.c
@@ -6,6 +6,10 @@ int main()
   {
     if (i % 3 == 0 && i % 5 == 0)
       printf("fizzbuzz\n");
+    else if (i % 3 == 0)
+      printf("fizz\n");
+    else if (i % 5 == 0)
+      printf("buzz\n");
     else
       printf("%d is not zz\n", i);
   }
